5c68f33127a8e66a9cbdf647cf8444ba4cff3d67
vicgriffin123 <vickamworkpro@gmail.com>
2024-07-11 17:59:15 +0300

    second commit

--- src/signup/Signup.jsx
+++ src/signup/Signup.jsx
@@ -1,32 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import {auth,provider} from "./config.jsx"
-import { signInWithPopup } from 'firebase/auth'
-import Home
- from '../page/home/Home.jsx';
+import React, { useEffect, useState } from 'react';
+import { auth, provider } from './config';
+import { signInWithPopup } from 'firebase/auth';
+import Home from '../page/home/Home';
 
 function Signup() {
-    const [value , setValue] =useState();
-    const handleClick = ()=>{
-        signInWithPopup(auth,provider).then((data)=>{
-            setValue(data.use.email)
-            localStorage.setItem('email' ,data.user.email)
-        })
-    }
-    useEffect(()=>{
-        setValue(localStorage.getItem("email"))
-    })
+  const [value, setValue] = useState(null);
 
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.email);
+      localStorage.setItem('email', data.user.email);
+    });
+  };
+
+  const handleFacebookLogin = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.email);
+      localStorage.setItem('email', data.user.email);
+    });
+  };
+
+  useEffect(() => {
+    const email = localStorage.getItem('email');
+    if (email) {
+      setValue(email);
+    }
+  }, []);
 
   return (
-    <div>
-        {value?<home/>:
-        <button onClick={handleClick}>
-        sign in with Google
-    </button>}
-    
-        
-    </div>
-  )
+    <>
+      <div className="wrapper">
+        <div className="container">
+          {value ? (
+            <Home />
+          ) : (
+            <>
+              <button className="btn-primary" onClick={handleFacebookLogin}>
+                Sign in with Facebook
+              </button>
+              <button onClick={handleGoogleLogin}>
+                Sign in with Google
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Signup
+export default Signup;

3a91e4600be159692c991bb2632fbdaeaea73a71
vicgriffin123 <vickamworkpro@gmail.com>
2024-07-11 18:08:57 +0300

    styling the app

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -8,6 +8,7 @@ function Home() {
   return (
     <div>
         <h1>Home</h1>
+        <p>hello dear friends am glad you were able to access this file to learn more on this call me through my number</p>
         <button onClick={logout}>logout</button>
     </div>
   )
